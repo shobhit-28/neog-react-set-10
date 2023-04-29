fe5f16c5411338b32472a30539b160131df168ba
shobhit-28 <shobhitraj34@gmail.com>
2023-04-29 13:15:43 +0530

    added empty inbox img

--- src/pages/inbox.jsx
+++ src/pages/inbox.jsx
@@ -2,13 +2,17 @@ import { useContext } from "react"
 import { FilterComponent } from "../components/filterComponent"
 import { MailContext } from "../contexts/mailContext"
 import { MailCardComponent } from "../components/mailCardComponent"
+import { emptyImages } from "../images/empty-images"
 
 export const Inbox = () => {
     const {filteredData} = useContext(MailContext)
     return (
         <>
             <FilterComponent/>
-            {filteredData.map((mailData) => <MailCardComponent mailData={mailData}key={mailData?.mId} /> )}
+            {filteredData.length > 0 ?
+            filteredData.map((mailData) => <MailCardComponent mailData={mailData}key={mailData?.mId} /> )
+            : 
+            <img className="empty-inbox-img" src={emptyImages.empty_inbox} alt="empty spam" />}
         </>
     )
 }
